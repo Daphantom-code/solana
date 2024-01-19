80a24011cf5da95160719b205550fc5a4578cdab
Daphantom-code <67028982+Daphantom-code@users.noreply.github.com>
2024-01-19 01:50:38 +0100

    Update native_token.rs

--- sdk/program/src/native_token.rs
+++ sdk/program/src/native_token.rs
@@ -16,9 +16,9 @@ pub fn sol_to_lamports(sol: f64) -> u64 {
 }
 
 use std::fmt::{Debug, Display, Formatter, Result};
-pub struct Mik(pub u64);
+pub struct Sol(pub u64);
 
-impl Mik {
+impl Sol {
     fn write_in_sol(&self, f: &mut Formatter) -> Result {
         write!(
             f,
@@ -29,13 +29,13 @@ impl Mik {
     }
 }
 
-impl Display for Mik {
+impl Display for Sol {
     fn fmt(&self, f: &mut Formatter) -> Result {
         self.write_in_sol(f)
     }
 }
 
-impl Debug for Mik {
+impl Debug for Sol {
     fn fmt(&self, f: &mut Formatter) -> Result {
         self.write_in_sol(f)
     }
